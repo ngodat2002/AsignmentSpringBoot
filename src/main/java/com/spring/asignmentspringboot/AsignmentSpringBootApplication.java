8bef908e63ef0e9e655b32e6832669d007e8ea0c
ngochithanhdat <ngochithanhdat02@gmmail.com>
2022-07-12 14:34:51 +0700

    Initial commit

---
+++ src/main/java/com/spring/asignmentspringboot/AsignmentSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.asignmentspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignmentSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsignmentSpringBootApplication.class, args);
+    }
+
+}
